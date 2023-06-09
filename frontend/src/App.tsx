5904b7c258b9444e1d94bd6ff4c20eaf95a22bee
lipezaballa <lipezaballa@gmail.com>
2023-06-09 17:03:40 +0200

    first changes to add hil-front in this repo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import useWebSocket from "react-use-websocket";
+import style from "./App.module.scss";
+import { TestControls } from "./TestControls/TestControls";
+
+const SERVER_URL = `${import.meta.env.VITE_SERVER_IP_HIL}:${
+    //FIXME: change to congif.toml
+    import.meta.env.VITE_SERVER_PORT_HIL
+}${import.meta.env.VITE_BACKEND_WEBSOCKET_PATH}`;
+
+const WEBSOCKET_URL = `ws://${SERVER_URL}`; //FIXME
+
+export type VehicleState = {
+    yDistance: number;
+    current: number;
+    duty: number;
+    temperature: number;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [vehicleState, setVehicleState] = useState<VehicleState>({
+        current: 0,
+        duty: 0,
+        temperature: 0,
+        yDistance: 0,
+    } as VehicleState);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const { sendMessage, sendJsonMessage, lastMessage, lastJsonMessage } =
+        useWebSocket(WEBSOCKET_URL, {
+            shouldReconnect: (closeEvent) => {
+                return true;
+            },
+        });
+
+    useEffect(() => {
+        if (lastJsonMessage !== null) {
+            const newVehicleState = lastJsonMessage as VehicleState;
+            if (newVehicleState.duty >= 0 && newVehicleState.duty < 256) {
+                setVehicleState(newVehicleState);
+            }
+        }
+    }, [lastJsonMessage]);
+
+    return (
+        <PageWrapper title="Testing">
+            <div className={style.testingPageWrapper}>
+                <TestControls
+                    sendJsonMessage={sendJsonMessage}
+                    sendMessage={sendMessage}
+                    lastMessage={lastMessage}
+                />
+                <div className={style.podRepresentation}>
+                    <div className={style.threeJSAndInfo}>
+                        <div className={style.threeJS}>
+                            <ThreeJsVehicle
+                                yDistance={vehicleState.yDistance}
+                            />
+                        </div>
+
+                        <div className={style.info}></div>
+                    </div>
+                    <div className={style.graphics}></div>
+                </div>
+            </div>
+        </PageWrapper>
+    );
 }
 
-export default App
+export default App;
