107875fef43e7e779145c2ba7ef70ceac36de7e2
lipezaballa <lipezaballa@gmail.com>
2023-06-12 12:30:10 +0200

    adding files from control-front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import useWebSocket from "react-use-websocket";
 import style from "./App.module.scss";
 import { TestControls } from "./TestControls/TestControls";
 import "common/dist/style.css"; //FIXME
+import { ThreeJsVehicle } from "./ThreeJs/ThreeJsVehicle";
 
 const SERVER_URL = `${import.meta.env.VITE_SERVER_IP_HIL}:${
     //FIXME: change to congif.toml
